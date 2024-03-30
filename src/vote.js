2b20b33803b3bc65396ed35fdce0a3fcc09e9aab
mako0523 <kurimako.0523@gmail.com>
2024-03-30 09:29:04 +0900

    fix: データベースの接続設定を変更

--- src/vote.js
+++ src/vote.js
@@ -4,26 +4,26 @@ const mysql = require("mysql");
 const app = express();
 
 const connection = mysql.createConnection({
-	host: "localhost",
-	user: "hokutofes_mako",
-	password: "ZUq8UDoMp6",
-	database: "hokutofes_vote",
-	port: 10022,
+  host: "sv14916.xserver.jp",
+  user: "hokutofes_user",
+  password: "ZUq8UDoMp6",
+  database: "hokutofes_vote",
+  port: 10022,
 });
 
 connection.connect((err) => {
-	if (err) {
-		console.log("error connecting: " + err.stack);
-		return;
-	}
-	console.log("success");
+  if (err) {
+    console.log("error connecting: " + err.stack);
+    return;
+  }
+  console.log("success");
 });
 
 app.get("/", (req, res) => {
-	connection.query("SELECT * FROM users", (error, results) => {
-		console.log(results);
-		res.render("hello.ejs");
-	});
+  connection.query("SELECT * FROM users", (error, results) => {
+    console.log(results);
+    res.render("hello.ejs");
+  });
 });
 
 app.listen(3000);
