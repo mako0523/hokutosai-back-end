a6609e85196d2e18cacd219223469bf6beaeae05
mako0523 <kurimako.0523@gmail.com>
2024-05-05 11:29:04 +0900

    feat: 模擬店の投票データを作成

---
+++ sql/stall.sql
@@ -0,0 +1,102 @@
+DELETE FROM
+    stall;
+
+INSERT INTO
+    stall (name)
+VALUES
+    ("チュチュチュロス（バレーボール部）");
+
+INSERT INTO
+    stall (name)
+VALUES
+    ("ポテト＆唐揚げ（ラグビー同好会）");
+
+INSERT INTO
+    stall (name)
+VALUES
+    ("剣道部アイス屋始めました（剣道部）");
+
+INSERT INTO
+    stall (name)
+VALUES
+    ("たこ焼きかった郎（漕艇部）");
+
+INSERT INTO
+    stall (name)
+VALUES
+    ("皐月茶会（茶道同好会）");
+
+INSERT INTO
+    stall (name)
+VALUES
+    ("ワッフル（バドミントン部）");
+
+INSERT INTO
+    stall (name)
+VALUES
+    ("命短シ食セヨ乙女（国際ビジネス学科４年）");
+
+INSERT INTO
+    stall (name)
+VALUES
+    ("ドリンクワンダーランド（北斗祭実行委員会）");
+
+INSERT INTO
+    stall (name)
+VALUES
+    ("焼きそば（テニス部）");
+
+INSERT INTO
+    stall (name)
+VALUES
+    ("ベビーカステラさん（男女バスケットボール部）");
+
+INSERT INTO
+    stall (name)
+VALUES
+    ("叙情苑（電子情報工学科４年有志）");
+
+INSERT INTO
+    stall (name)
+VALUES
+    ("ぎゃるまみれ43（国際ビジネス学科５年）");
+
+INSERT INTO
+    stall (name)
+VALUES
+    ("焼き鳥、フランクフルト（機関学同好会）");
+
+INSERT INTO
+    stall (name)
+VALUES
+    ("シュガーバターのすゝめ（電子情報工学科４年）");
+
+INSERT INTO
+    stall (name)
+VALUES
+    ("ポップコーン（ヨット部）");
+
+INSERT INTO
+    stall (name)
+VALUES
+    ("あんばやし（野球部）");
+
+INSERT INTO
+    stall (name)
+VALUES
+    ("君にドーナツを揚げる（フリースタイルダンス部）");
+
+INSERT INTO
+    stall (name)
+VALUES
+    ("似顔絵（美術同好会）");
+
+INSERT INTO
+    stall (name)
+VALUES
+    ("焼き餃子（サッカー部）");
+
+INSERT INTO
+    stall (name)
+VALUES
+    ("パパ＆ママ（富山高専後援会）");
