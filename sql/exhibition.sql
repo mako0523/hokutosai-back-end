d99b1d6def7eda59c1d464fa913b931cf66528a7
mako0523 <kurimako.0523@gmail.com>
2024-05-05 20:12:43 +0900

    feat: 展示の投票データを作成

---
+++ sql/exhibition.sql
@@ -0,0 +1,94 @@
+DELETE FROM
+    exhibition;
+
+INSERT INTO
+    exhibition (name)
+VALUES
+    ("フォトスポット（１年生）");
+
+INSERT INTO
+    exhibition (name)
+VALUES
+    ("謎解き（２年生）");
+
+INSERT INTO
+    exhibition (name)
+VALUES
+    ("謎解きすごろくゲーム（電子情報工学科２年生）");
+
+INSERT INTO
+    exhibition (name)
+VALUES
+    ("すごろく（国際ビジネス学科２年生）");
+
+INSERT INTO
+    exhibition (name)
+VALUES
+    ("謎解き＆体験型ゲーム（商船学科２年生）");
+
+INSERT INTO
+    exhibition (name)
+VALUES
+    ("Ti-TEAMでまとめたものの展示（２年生）");
+
+INSERT INTO
+    exhibition (name)
+VALUES
+    ("タイのお化け紹介（タイ留学生）");
+
+INSERT INTO
+    exhibition (name)
+VALUES
+    ("ゲームセンター（電子情報工学科３年生）");
+
+INSERT INTO
+    exhibition (name)
+VALUES
+    ("迷路（国際ビジネス学科３年生）");
+
+INSERT INTO
+    exhibition (name)
+VALUES
+    ("お化け屋敷（商船学科３年生）");
+
+INSERT INTO
+    exhibition (name)
+VALUES
+    ("ゲーム展示、デジタルイラスト展示など（デジタルメディア創作部）");
+
+INSERT INTO
+    exhibition (name)
+VALUES
+    ("新聞の展示（新聞部）");
+
+INSERT INTO
+    exhibition (name)
+VALUES
+    ("読書スペースの設置、部誌の頒布（文芸同好会）");
+
+INSERT INTO
+    exhibition (name)
+VALUES
+    ("書道作品展示（書道同好会）");
+
+INSERT INTO
+    exhibition (name)
+VALUES
+    ("写真展示（写真同好会）");
+
+INSERT INTO
+    exhibition (name)
+VALUES
+    ("鉄道模型を用いたジオラマの展示（鉄道部）");
+
+INSERT INTO
+    exhibition (name)
+VALUES
+    (
+        "ロボコンに出場したロボット２台の展示、及び希望者へのロボット操縦体験（メカトロ技術研究部・メカテック部）"
+    );
+
+INSERT INTO
+    exhibition (name)
+VALUES
+    ("船の写真展示（富山商船OB）");
