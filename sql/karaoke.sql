d1aa2b619c96f5fc849ebd65c8125250aa12de82
mako0523 <kurimako.0523@gmail.com>
2024-05-15 14:08:21 +0900

    feat: カラオケの出場者のデータ入力

---
+++ sql/karaoke.sql
@@ -0,0 +1,37 @@
+DELETE FROM
+    karaoke;
+
+INSERT INTO
+    karaoke (name)
+VALUES
+    ("篠原颯太（商船学科４年）");
+
+INSERT INTO
+    karaoke (name)
+VALUES
+    ("川口直輝（国際ビジネス学科４年）");
+
+INSERT INTO
+    karaoke (name)
+VALUES
+    ("小林颯太（国際ビジネス学科３年）");
+
+INSERT INTO
+    karaoke (name)
+VALUES
+    ("小田嶋真寿（商船学科２年）");
+
+INSERT INTO
+    karaoke (name)
+VALUES
+    ("渡邉嵐丸（商船学科４年）");
+
+INSERT INTO
+    karaoke (name)
+VALUES
+    ("小林莉音（国際ビジネス学科２年）");
+
+INSERT INTO
+    karaoke (name)
+VALUES
+    ("河野真央（国際ビジネス学科２年）");
