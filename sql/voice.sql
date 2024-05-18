9a1d5653d00be3b6e33e0d63f45c655380ee94bb
mako0523 <kurimako.0523@gmail.com>
2024-05-18 10:05:48 +0900

    chore: Nice voiceコンテストのデータ入力

--- sql/voice.sql
+++ sql/voice.sql
@@ -4,4 +4,34 @@ DELETE FROM
 INSERT INTO
     voice (name)
 VALUES
-    ("hoge");
+    ("みやまのくわがた（鵜野倖輔）");
+
+INSERT INTO
+    voice (name)
+VALUES
+    ("ぼたもち（吉田美優）");
+
+INSERT INTO
+    voice (name)
+VALUES
+    ("もきゅあ（福島里桜）");
+
+INSERT INTO
+    voice (name)
+VALUES
+    ("しんたん（岡本美子）");
+
+INSERT INTO
+    voice (name)
+VALUES
+    ("恥飛子（西野美悠）");
+
+INSERT INTO
+    voice (name)
+VALUES
+    ("ぎゃるみょん（齋藤優羽）");
+
+INSERT INTO
+    voice (name)
+VALUES
+    ("山村先生");
